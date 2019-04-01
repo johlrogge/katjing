b9fc9e1183c79c19d84dd5c932e5c6c90ad5d247
Joakim Ohlrogge <joakim.ohlrogge@gmail.com>
2019-04-01 23:51:10 +0200

    Create amounts

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,8 @@
 //! [Amount]: struct.Amount.html
 
 #[doc(html_playground_url = "https://play.rust-lang.org/")]
-
 use core::fmt::Debug;
 use core::marker::PhantomData;
-
 
 /// Represents currency. Mainly to keep money in different currencies as separate types that cannot be used together without conversion
 pub trait Currency
@@ -27,11 +25,18 @@ where
         Self: Sized,
 {
         /// creates an instance of money in this currency
-        fn create<V>(value: V) -> Money<V, Self>
+        fn create_money<V>(value: V) -> Money<V, Self>
         where
                 V: MoneyValue,
         {
                 Money(value, PhantomData::<Self>)
+        }
+
+        fn create_amount<V>(value: V) -> Amount<V, Self>
+        where
+                V: AmountValue,
+        {
+                Amount(value, PhantomData::<Self>)
         }
 }
 
@@ -56,7 +61,7 @@ where
         /// use katjing::{Money, Currency, currencies};
         /// currencies!(Eur);
         ///
-        /// let eur_12 = Eur::create(12);
+        /// let eur_12 = Eur::create_money(12);
         /// assert_eq!(eur_12, eur_12);
         ///```
         fn eq(&self, other: &Self) -> bool {
@@ -65,11 +70,30 @@ where
 }
 
 /// An amount represents something that can be paid. Like a Fee, Price, Shipping or Amortization
-pub struct Amount<M, V, C> (V, PhantomData<C>, PhantomData<M>)
+#[derive(Debug)]
+pub struct Amount<AV, C>(AV, PhantomData<C>)
 where
-    M:MoneyValue + Into<V>,
-    V:AmountValue<M>,
-    C:Currency ;
+        AV: AmountValue,
+        C: Currency;
+
+impl<V, C> PartialEq<Amount<V, C>> for Amount<V, C>
+where
+        V: MoneyValue,
+        C: Currency,
+{
+        /// ```
+        /// use katjing::{Amount, Currency, currencies};
+        /// currencies!(Eur);
+        ///
+        /// let eur_12 = Eur::create_amount(12);
+        /// let eur_13 = Eur::create_amount(13);
+        /// assert_eq!(eur_12, eur_12);
+        /// assert_ne!(eur_13, eur_12);
+        /// ```
+        fn eq(&self, other: &Self) -> bool {
+                self.0 == other.0
+        }
+}
 
 
 /// MoneyValue is just a collection of traits needed to properly represent a monetary value. It is implemented via a blanket implementation on all types that implement all the needed traits. You should never need to implement MoneyValue directly.
@@ -81,20 +105,15 @@ where
 /// Blanket implementation of MoneyValue
 impl<T> MoneyValue for T where T: Sized + Debug + PartialEq<T> {}
 
-
 /// AmountValue is just a collection of traits needed to properly represent a monetary value. It is implemented via a blanket implementation on all typs that implement all the needed traits. You should never need to implement MoneyValue directly.
-pub trait AmountValue<MV>
+pub trait AmountValue
 where
-    MV:MoneyValue + Into<Self>,
-    Self: MoneyValue + Into<MV> {
+        Self: MoneyValue,
+{
 }
 
 /// Blanket implementation of AmountValue
-impl<T, MV> AmountValue<MV> for T where
-    T: MoneyValue + Into<MV>,
-    MV: MoneyValue + Into<T>,
-{
-}
+impl<T> AmountValue for T where T: MoneyValue {}
 
 #[macro_export]
 macro_rules! currencies {
@@ -108,20 +127,44 @@ macro_rules! currencies {
 
 #[cfg(test)]
 pub mod test {
-        use crate::{Currency, Money};
+        use crate::Currency;
         currencies!(Eur, Sek);
-        #[test]
-        fn create_money_from_currency() {
-                let eur_47 = Eur::create(47);
-                let eur_11 = Eur::create(11);
+        mod money {
+                use super::Eur;
+                use crate::{Currency, Money};
+                #[test]
+                fn create_from_currency() {
+                        let eur_47 = Eur::create_money(47);
+                        let eur_11 = Eur::create_money(11);
 
-                assert_eq!(eur_47, eur_47);
-                assert_ne!(eur_11, eur_47);
+                        assert_eq!(eur_47, eur_47);
+                        assert_ne!(eur_11, eur_47);
+                }
+
+                #[test]
+                fn is_not_larger_than_value() {
+                        assert!(std::mem::size_of::<Money<i32, Eur>>()
+                                == std::mem::size_of::<i32>());
+                }
         }
 
-        #[test]
-        fn money_is_not_larger_than_value() {
-                assert!(std::mem::size_of::<Money<i32, Eur>>() == std::mem::size_of::<i32>());
-        }
+        mod amount {
+                use super::Eur;
+                use crate::{Amount, Currency};
 
+                #[test]
+                fn create_from_currency() {
+                        let eur_47 = Eur::create_amount(47);
+                        let eur_11 = Eur::create_amount(11);
+
+                        assert_eq!(eur_47, eur_47);
+                        assert_ne!(eur_47, eur_11);
+                }
+
+                #[test]
+                fn is_not_larger_than_value() {
+                        assert!(std::mem::size_of::<Amount<i32, Eur>>()
+                                == std::mem::size_of::<i32>());
+                }
+        }
 }
